5b30f19d8b0cdf44f8a70e053133927e7960147e
Floreno12 <gajdajdenis500@gmail.com>
2024-05-03 08:08:06 +0200

    Test

---
+++ TestingNew.js
@@ -0,0 +1,11 @@
+function test (){
+var testing = 'work like expecte';
+if(testing == 'work like expected'){
+    console.log(testing);
+}else{
+    console.log('under its place');
+}
+
+}
+
+test();
